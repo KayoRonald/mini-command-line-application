ac700f58bca0fb17d5d9da7abba6308d4777513b
Kayo Ronald <kayoronald4321@gmail.com>
2024-04-28 13:14:51 +0000

    feat: get ip command line

--- app/app.go
+++ app/app.go
@@ -1,6 +1,8 @@
 package app
 
 import (
+	"command-line/utils"
+
 	"github.com/urfave/cli"
 )
 
@@ -9,5 +11,18 @@ func Gerar() *cli.App {
 	app.Name = "Aplicação de linha de comando"
 	app.Usage = "Buscar IPs e nomes de servidores"
 
+	app.Commands = []cli.Command{
+		{
+			Name:  "ip",
+			Usage: "buscar endereços IPS na internet",
+			Flags: []cli.Flag{
+				cli.StringFlag{
+					Name:  "host",
+					Value: "golang.org",
+				},
+			},
+			Action: utils.BuscarIp,
+		},
+	}
 	return app
 }
--- utils/buscarIp.go
+++ utils/buscarIp.go
@@ -1,1 +1,20 @@
 package utils
+
+import (
+	"fmt"
+	"log"
+	"net"
+
+	"github.com/urfave/cli"
+)
+
+func BuscarIp(c *cli.Context) {
+	host := c.String("host")
+	ips, erro := net.LookupIP(host)
+	if erro != nil {
+		log.Fatal(erro)
+	}
+	for _, ip := range ips {
+		fmt.Println(ip)
+	}
+}
